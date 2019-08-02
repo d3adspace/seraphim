642f64dd6792d78961de18d38fe19a59d4a1004f
Felix Klauke <info@felix-klauke.de>
2019-08-02 11:28:58 +0200

    Add factory for bootstrapping instantiation.

--- server/src/main/java/de/d3adspace/seraphim/server/SeraphimServerBootstrap.java
+++ server/src/main/java/de/d3adspace/seraphim/server/SeraphimServerBootstrap.java
@@ -3,7 +3,7 @@ package de.d3adspace.seraphim.server;
 public class SeraphimServerBootstrap {
 
   public static void main(String[] args) {
-    SimpleSeraphimServer simpleSeraphimServer = new SimpleSeraphimServer("0.0.0.0", 8080);
-    simpleSeraphimServer.start();
+    SeraphimServer seraphimServer = SeraphimServerFactory.createServer("0.0.0.0", 8080);
+    seraphimServer.start();
   }
 }
